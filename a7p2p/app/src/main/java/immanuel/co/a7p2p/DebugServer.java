707fcfe5ebd72d057ab0f52b7c3eb2faca0370c5
Immanuel Raj <raj@immanuel.co>
2018-09-14 09:48:25 +0530

    changes

---
+++ a7p2p/app/src/main/java/immanuel/co/a7p2p/DebugServer.java
@@ -0,0 +1,31 @@
+package immanuel.co.a7p2p;
+
+import java.util.Map;
+
+import fi.iki.elonen.NanoHTTPD;
+
+/**
+ * Created by Mikhael LOPEZ on 14/12/2015.
+ */
+public class DebugServer extends NanoHTTPD {
+
+    public DebugServer(int port) {
+        super(port);
+    }
+
+    public DebugServer(String hostname, int port) {
+        super(hostname, port);
+    }
+
+    @Override
+    public Response serve(IHTTPSession session) {
+        String msg = "<html><body><h1>Hello server</h1>\n";
+        Map<String, String> parms = session.getParms();
+        if (parms.get("username") == null) {
+            msg += "<form action='?' method='get'>\n  <p>Your name: <input type='text' name='username'></p>\n" + "</form>\n";
+        } else {
+            msg += "<p>Hello, " + parms.get("username") + "!</p>";
+        }
+        return newFixedLengthResponse( msg + "</body></html>\n" );
+    }
+}
---
+++ co.immanuel.webcast/app/src/main/java/webcast/immanuel/co/immanuelwebcast/MainActivity.java
@@ -0,0 +1,32 @@
+package webcast.immanuel.co.immanuelwebcast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView wv1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        wv1=(WebView)findViewById(R.id.webview);
+        wv1.setWebViewClient(new MyBrowser());
+        wv1.getSettings().setLoadsImagesAutomatically(true);
+        wv1.getSettings().setJavaScriptEnabled(true);
+        wv1.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        wv1.loadUrl("http://webcast.immanuel.co/Audio_.html");
+    }
+
+    private class MyBrowser extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+}
